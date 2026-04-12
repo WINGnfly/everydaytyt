7e00bf973d0b76b76c19f4a25b8fdcf21ff5a8d4
WING <93694305+WINGnfly@users.noreply.github.com>
2026-04-12 09:21:54 +0700

    Update auto_upload.py

--- auto_upload.py
+++ auto_upload.py
@@ -170,7 +170,7 @@ def create_page(cookie: str, cookie_cf_clearance: str):
     # KHÔNG dùng co.headless(True) nếu chạy bằng Xvfb (để bypass Cloudflare tốt hơn)
     # Nếu không dùng Xvfb ở YML, hãy bật dòng dưới đây:
     # co.headless(True)
-    co.set_argument(--headless=new)
+    co.set_argument('--headless=new')
 
     # Các tham số bắt buộc cho môi trường Linux/Docker/GitHub Actions
     co.set_argument('--no-sandbox')
